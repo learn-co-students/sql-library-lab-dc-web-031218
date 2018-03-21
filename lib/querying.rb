d3504699e7605681511dc19c04aa46316aed3243
azeelleeza <leezaluncheon@gmail.com>
2018-03-21 14:29:35 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,16 +12,40 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors LEFT OUTER JOIN subgenres ON authors.id = subgenres.id"######?????????????
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id GROUP BY series.title HAVING characters.species = \"human\""
-# HAVING VS WHERE
+  "SELECT series.title
+  FROM series
+  JOIN characters
+  ON series.id = characters.series_id
+  WHERE characters.species = \"human\"
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1"
 
+  # "SELECT series.title
+  # FROM series
+  # JOIN characters
+  # ON series.id = characters.series_id
+  # GROUP BY series.title
+  # HAVING characters.species = \"human\""
+# HAVING VS WHERE
+## Solution uses two joins?
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.id) FROM characters JOIN character_books WHERE characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.id) DESC"
-  # ORDER BY WRONG
+  "SELECT characters.name, COUNT(character_books.id)
+  FROM characters
+  JOIN character_books
+  WHERE characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(character_books.id) DESC"
+  # ORDER BY REQUIRED?
 end
